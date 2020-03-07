dc0e016911a28e252fc9d1ae01a3732e19cef96d
Praveen Kumar Srinivasan <praveen.sxi@gmail.com>
2020-03-07 13:35:13 +1300

    script clean up

--- bin/init_zsh.sh
+++ bin/init_zsh.sh
@@ -1,9 +1,3 @@
-mkdir -p ~/.dotfiles/pks/
-
-# clone dotfiles_min to ~/.dotfiles/pks/
-cd ~/.dotfiles/pks/
-git clone https://github.com/praveenkumarsrinivasan/dotfiles_min.git
-
 # install zsh custom files
 cd ~/.oh-my-zsh/custom/
 ln -s ~/.dotfiles/pks/zsh/*.zsh
