3e642ea7169ffeb8e1c8d62500327d57ef7203c0
Praveen Kumar Srinivasan <praveen.sxi@gmail.com>
2020-03-07 22:56:32 +1300

    adding zshrc

--- zsh/custom/docker_alias.zsh
+++ zsh/custom/docker_alias.zsh
@@ -8,3 +8,10 @@ alias dcup='docker-compose up -d'
 alias dcdn='docker-compose down'
 alias dcps='docker-compose ps'
 alias dclogs='docker-compose logs'
+
+# Stop and remove all comtainers
+#docker stop $(docker ps -a -q)
+#docker rm $(docker ps -a -q)
+
+# Remove all images
+#docker image prune -a
